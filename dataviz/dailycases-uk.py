ae23542d087cae041775fdf93ead022341edcf26
Rocco Meli <r.meli@bluemail.ch>
2020-03-13 23:38:23 +0000

    new and total cases uk

---
+++ dataviz/dailycases-uk.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+import itertools
+import os
+
+datapath = "data/UK/"
+fname = "DailyConfirmedCases.xlsx"
+
+plotpath ="dataviz/plots"
+
+dailycases = pd.read_excel(os.path.join(datapath, fname))
+
+dailycases.rename(columns={"DateVal": "date", "CMODateCount": "new", "CumCases": "total"}, inplace=True)
+
+dailycases["date"] = dailycases["date"].apply(pd.to_datetime).dt.date
+
+print(dailycases.describe())
+
+
+def plotdailycases(yname, log=False, xskyp=2):
+    plt.figure()
+
+    sns.barplot(x="date", y=yname, data=dailycases, palette="rocket_r")
+
+    plt.xlabel("")
+    plt.ylabel(f"{yname.capitalize()} Cases (UK)")
+
+    locs, labels = plt.xticks(rotation=90)
+    plt.xticks(locs[::xskyp], labels[::xskyp])
+
+    plt.tight_layout()
+    plt.savefig(os.path.join(plotpath, f"{yname}_cases_uk{'_log' if log else ''}.pdf"))
+
+
+for yname, log in itertools.product(("new", "total"), (True, False)):
+    plotdailycases(yname, log)
