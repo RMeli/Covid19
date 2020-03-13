397a4379f46f0559663f17c28c5667f71e108f5f
Rocco Meli <r.meli@bluemail.ch>
2020-03-13 23:48:00 +0000

    fix log and add start date

--- dataviz/dailycases-uk.py
+++ dataviz/dailycases-uk.py
@@ -3,27 +3,37 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 
+import datetime
 import itertools
 import os
 
 datapath = "data/UK/"
 fname = "DailyConfirmedCases.xlsx"
 
-plotpath ="dataviz/plots"
+plotpath = "dataviz/plots"
 
 dailycases = pd.read_excel(os.path.join(datapath, fname))
 
-dailycases.rename(columns={"DateVal": "date", "CMODateCount": "new", "CumCases": "total"}, inplace=True)
+dailycases.rename(
+    columns={"DateVal": "date", "CMODateCount": "new", "CumCases": "total"},
+    inplace=True,
+)
 
 dailycases["date"] = dailycases["date"].apply(pd.to_datetime).dt.date
 
 print(dailycases.describe())
 
 
-def plotdailycases(yname, log=False, xskyp=2):
+def plotdailycases(yname, log=False, xskyp=2, start_date=datetime.date(2020, 2, 22)):
     plt.figure()
 
-    sns.barplot(x="date", y=yname, data=dailycases, palette="rocket_r")
+    sns.barplot(
+        log=log,
+        x="date",
+        y=yname,
+        data=dailycases[dailycases.date >= start_date],
+        palette="rocket_r",
+    )
 
     plt.xlabel("")
     plt.ylabel(f"{yname.capitalize()} Cases (UK)")
